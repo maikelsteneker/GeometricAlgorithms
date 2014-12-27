d91d67085aebcb32ac3868996bee0a2fcd8a8658
Maikel Steneker <maikel.steneker@gmail.com>
2014-12-27 22:11:30 +0100

    Added reading polygons from file.

--- src/nl/tue/win/ga/io/ReadPolygonFromFile.java
+++ src/nl/tue/win/ga/io/ReadPolygonFromFile.java
@@ -5,17 +5,21 @@
  */
 package nl.tue.win.ga.io;
 
+import java.awt.Point;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.regex.Pattern;
 import nl.tue.win.ga.model.SimplePolygon;
 
 /**
- * Reads a polygon from a file in the file format specified for the 2IL55 course.
- * 
+ * Reads a polygon from a file in the file format specified for the 2IL55
+ * course.
+ *
  * first line contains one integer: the number of vertices of the simple polygon
  *
  * remaining lines contain two positive integers each: the two coordinates of a
@@ -25,6 +29,9 @@ import nl.tue.win.ga.model.SimplePolygon;
  */
 public class ReadPolygonFromFile {
 
+    final static Pattern FIRST_LINE = Pattern.compile("\\d*");
+    final static Pattern REMAINING_LINES = Pattern.compile("\\d*[ ]+\\d*");
+
     public static SimplePolygon readPolygonFromFile(File file)
             throws IOException {
         if (!file.exists()) {
@@ -32,18 +39,43 @@ public class ReadPolygonFromFile {
         }
         return readInput(new FileReader(file));
     }
-    
+
     public static SimplePolygon readPolygonFromFile(String filename)
             throws IOException {
         return readPolygonFromFile(new File(filename));
     }
-    
+
     public static SimplePolygon readPolygonFromStream(InputStream input)
             throws IOException {
         return readInput(new InputStreamReader(input));
     }
 
     private static SimplePolygon readInput(Reader input) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        BufferedReader buffer = new BufferedReader(input);
+        int nVertices = readFirstLine(buffer);
+        Point[] vertices = new Point[nVertices];
+        for (int i = 0; i < vertices.length; i++) {
+            vertices[i] = readLine(buffer);
+        }
+        return new SimplePolygon(vertices);
+    }
+
+    private static int readFirstLine(BufferedReader input) throws IOException {
+        String currentLine = input.readLine();
+        if (!FIRST_LINE.matcher(currentLine).matches()) {
+            throw new IOException("First line does not contain a single integer!");
+        }
+        return Integer.parseInt(currentLine);
+    }
+
+    private static Point readLine(BufferedReader input) throws IOException {
+        String currentLine = input.readLine();
+        if (!REMAINING_LINES.matcher(currentLine).matches()) {
+            throw new IOException("Line does not contain two positive integers!");
+        }
+        String[] tokens = currentLine.split("[ ]+");
+        int x = Integer.parseInt(tokens[0]);
+        int y = Integer.parseInt(tokens[1]);
+        return new Point(x, y);
     }
 }
