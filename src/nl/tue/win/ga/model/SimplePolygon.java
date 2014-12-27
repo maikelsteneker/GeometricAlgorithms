1744bd262a49f75d198428cb93c40c2e7b6dc9a3
Maikel Steneker <maikel.steneker@gmail.com>
2014-12-27 17:22:21 +0100

    Added invariant to check whether polygon is simple.

--- src/nl/tue/win/ga/model/SimplePolygon.java
+++ src/nl/tue/win/ga/model/SimplePolygon.java
@@ -51,4 +51,25 @@ public class SimplePolygon implements Iterable<Point> {
             prev = p;
         }
     }
+
+    public boolean invariant() {
+        // invariant: polygon is simple
+        // we check this by computing the cross product between consecutive edges
+        // these should all have the same sign
+        int prev = 0; // previous cross product
+        if (hull.length < 2) {
+            return true;
+        }
+        for (int i = 0; i < hull.length - 2; i++) {
+            int dx1 = hull[i + 1].x - hull[i].x;
+            int dy1 = hull[i + 1].y - hull[i].y;
+            int dx2 = hull[i + 2].x - hull[i + 1].x;
+            int dy2 = hull[i + 2].y - hull[i + 1].y;
+            int crossProduct = dx1 * dy2 - dy1 * dx2;
+            if ((crossProduct < 0) == (prev < 0) && i > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
