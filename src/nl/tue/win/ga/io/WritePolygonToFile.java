e4a500884e9bf9be7500f1cdb43be313dcbd9092
Maikel Steneker <maikel.steneker@gmail.com>
2014-12-27 22:24:02 +0100

    Added writing to file.

--- src/nl/tue/win/ga/io/WritePolygonToFile.java
+++ src/nl/tue/win/ga/io/WritePolygonToFile.java
@@ -1,7 +1,9 @@
 package nl.tue.win.ga.io;
 
 import java.awt.Point;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import nl.tue.win.ga.model.SimplePolygon;
@@ -24,17 +26,40 @@ public class WritePolygonToFile {
         writePolygonToFile(file, polygon);
     }
 
-    public static void writePolygonToFile(String filename, SimplePolygon polygon) throws IOException {
+    public static void writePolygonToFile(String filename, SimplePolygon polygon)
+            throws IOException {
         File file = new File(filename);
         writePolygonToFile(file, polygon);
     }
 
-    public static void writePolygonToFile(File file, List<Point> hull) throws IOException {
+    public static void writePolygonToFile(File file, List<Point> hull)
+            throws IOException {
         SimplePolygon polygon = new SimplePolygon(hull);
         writePolygonToFile(file, polygon);
     }
 
-    public static void writePolygonToFile(File file, SimplePolygon polygon) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static void writePolygonToFile(File file, SimplePolygon polygon)
+            throws IOException {
+        FileWriter fstream = new FileWriter(file);
+        BufferedWriter out = new BufferedWriter(fstream);
+        
+        writeFirstLine(out, polygon);
+        writeRemainingLines(out, polygon);
+        out.close();
+    }
+
+    private static void writeFirstLine(BufferedWriter out, SimplePolygon polygon)
+            throws IOException {
+        out.write(Integer.toString(polygon.size()));
+        out.newLine();
+    }
+
+    private static void writeRemainingLines(BufferedWriter out,
+            SimplePolygon polygon) throws IOException {
+        for (Point p : polygon) {
+            String line = p.x + " " + p.y;
+            out.write(line);
+            out.newLine();
+        }
     }
 }
