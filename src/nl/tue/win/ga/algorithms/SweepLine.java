99619ef35a3cfb014e7d260a3173faa6bc26a201
CoenHamers <coenhamers@gmail.com>
2015-01-07 17:26:25 +0100

    Added a sweep line trapezoidal map. Does not work perfectly yet :D Signed-off-by: CoenHamers <coenhamers@gmail.com>

--- src/nl/tue/win/ga/algorithms/SweepLine.java
+++ src/nl/tue/win/ga/algorithms/SweepLine.java
@@ -1,14 +1,127 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.tue.win.ga.algorithms;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import nl.tue.win.ga.model.*;
+import java.util.TreeMap;
 
 /**
  *
  * @author Administrator
  */
 public class SweepLine {
-    
+
+    private List<Point> SortedPoints;
+    private BinarySearchTree Bst;
+    private TreeMap SearchTree = new TreeMap();
+    private List<LineSegment> Verticals = new ArrayList<>();
+    private List<LineSegment> edges = new ArrayList<>();
+
+    public SweepLine(ArrayList<Point> points) {
+        MergeSort ms = new MergeSort();
+        SortedPoints = ms.sort(points);
+        initializeEdges(points);
+        Bst = new BinarySearchTree();
+
+    }
+
+    public void sweep() {
+        for (Point p : SortedPoints) {
+            Point below = null;
+            Point above = null;
+            SearchTree.put(p.y, p);
+            
+            Point begin = getBeginPoint(p);
+            if (begin != null) {
+                SearchTree.remove(begin.y);
+            }
+            
+            if (SearchTree.lowerKey(p.y) != null) {
+                below = (Point) SearchTree.get(SearchTree.lowerKey(p.y));
+            }
+            if (SearchTree.higherKey(p.y) != null) {
+                above = (Point) SearchTree.get(SearchTree.higherKey(p.y));
+            }
+
+            LineSegment ls1;
+            LineSegment ls2;
+
+            if (below != null) {
+                Point intersect1 = calculateIntersection(p, below);
+                ls1 = new LineSegment(p, new Point(intersect1.x, intersect1.y), null);
+            } else {
+                ls1 = new LineSegment(p, new Point(p.x, p.y - 5000), null);
+            }
+            if (above != null) {
+                Point intersect2 = calculateIntersection(p, above);
+                ls2 = new LineSegment(p, new Point(intersect2.x, intersect2.y), null);
+            } else {
+                ls2 = new LineSegment(p, new Point(p.x, p.y + 5000), null);
+            }
+
+            Verticals.add(ls1);
+            Verticals.add(ls2);
+        }
+    }
+
+    public List<LineSegment> getVerticals() {
+        return Verticals;
+    }
+
+    private void initializeEdges(List<Point> hull) {
+
+        Point prev = null;
+        for (Point p : hull) {
+            if (prev == null) {
+                prev = p;
+            } else {
+                Face f = null;//p.x > prev.x ? outside : inside;
+                LineSegment l;
+                if (p.x > prev.x) {
+                    l = new LineSegment(prev, p, f);
+                } else {
+                    l = new LineSegment(p, prev, f);
+                }
+                edges.add(l);
+                prev = p;
+            }
+        }
+        Point begin = hull.get(0);
+        Point end = hull.get(hull.size() - 1);
+        LineSegment closingSegment;
+        if (begin.x > end.x) {
+            closingSegment = new LineSegment(end, begin, null);
+        } else {
+            closingSegment = new LineSegment(begin, end, null);
+        }
+        edges.add(closingSegment);
+    }
+
+    private Point getBeginPoint(Point end) {
+        for (LineSegment edge : edges) {
+            if (edge.getEndPoint().x == end.x && edge.getEndPoint().y == end.y) {
+
+                return edge.getStartPoint();
+            }
+        }
+        return null;
+    }
+
+    private Point calculateIntersection(Point p, Point start) {
+        Point end = null;
+        for (LineSegment edge : edges) {
+            if (edge.getStartPoint().x == start.x && edge.getStartPoint().y == start.y) {
+                end = new Point(edge.getEndPoint().x, edge.getEndPoint().y);
+                break;
+            }
+        }
+        if (end != null) {
+            double slope = ( (double )start.y - (double) end.y) / ( (double) start.x - (double) end.x);
+            double y = start.y + slope * p.x - slope * start.x;
+            return new Point(p.x, (int) y);
+        }
+
+        return null;
+    }
 }
--- src/nl/tue/win/ga/gui/DrawInterface.java
+++ src/nl/tue/win/ga/gui/DrawInterface.java
@@ -14,7 +14,8 @@ import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import nl.tue.win.ga.io.ReadPolygonFromFile;
 import nl.tue.win.ga.io.WritePolygonToFile;
-import nl.tue.win.ga.model.SimplePolygon;
+import nl.tue.win.ga.model.*;
+import nl.tue.win.ga.algorithms.*;
 
 /**
  * GUI for drawing input files for DBL Algorithms
@@ -23,11 +24,12 @@ import nl.tue.win.ga.model.SimplePolygon;
  */
 public class DrawInterface extends javax.swing.JFrame {
 
-    List<Point> points = new ArrayList<>();
+    ArrayList<Point> points = new ArrayList<>();
     File outputFile;
     final int MAX_POINTS = 100000;
     final static String EXTENSION = "txt";
     int selected = -1;
+    List<LineSegment> segments = new ArrayList<>();
 
     /**
      * Creates new form DrawInterface
@@ -79,6 +81,7 @@ public class DrawInterface extends javax.swing.JFrame {
             jButton3 = new javax.swing.JButton();
             jCheckBox1 = new javax.swing.JCheckBox();
             jCheckBox2 = new javax.swing.JCheckBox();
+            jButton4 = new javax.swing.JButton();
 
             setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -105,7 +108,7 @@ public class DrawInterface extends javax.swing.JFrame {
             );
             jPanel1Layout.setVerticalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 128, Short.MAX_VALUE)
+                .addGap(0, 149, Short.MAX_VALUE)
             );
 
             jTextFieldMin.setText("3");
@@ -165,6 +168,13 @@ public class DrawInterface extends javax.swing.JFrame {
                 }
             });
 
+            jButton4.setText("jButton4");
+            jButton4.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton4ActionPerformed(evt);
+                }
+            });
+
             javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
             getContentPane().setLayout(layout);
             layout.setHorizontalGroup(
@@ -202,9 +212,12 @@ public class DrawInterface extends javax.swing.JFrame {
                                             .addComponent(jCheckBox1)
                                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                             .addComponent(jCheckBox2)))
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 242, Short.MAX_VALUE)
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                            .addComponent(jButton4)
+                                            .addGap(31, 31, 31)
+                                            .addComponent(jButton1))
                                         .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING))))))
                     .addContainerGap())
             );
@@ -220,7 +233,8 @@ public class DrawInterface extends javax.swing.JFrame {
                         .addComponent(jTextFieldMax, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel1)
                         .addComponent(jLabel2)
-                        .addComponent(jLabel5))
+                        .addComponent(jLabel5)
+                        .addComponent(jButton4))
                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton2)
@@ -328,6 +342,7 @@ public class DrawInterface extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         points.clear();
+        segments.clear();
         repaint();
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -367,11 +382,23 @@ public class DrawInterface extends javax.swing.JFrame {
         repaint();
     }//GEN-LAST:event_jCheckBox2ActionPerformed
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        SweepLine sl =  new SweepLine(points);
+        sl.sweep();
+        segments = sl.getVerticals();
+        repaint();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
     private void paintMainPanel(javax.swing.JPanel panel, Graphics g) {
         SimplePolygon polygon = new SimplePolygon(points);
         polygon.draw(g, jCheckBox1.isSelected(), jCheckBox2.isSelected());
+        for(LineSegment ls: segments) {
+            g.drawLine(ls.getStartPoint().x, ls.getStartPoint().y, ls.getEndPoint().x, ls.getEndPoint().y);
+        }
         jLabel5.setText("Number of points: " + this.points.size()
                 + (!polygon.invariant() ? "\t Invariant violated!!!" : ""));
+        
+        
     }
 
     private Point generateRandomPoint(int x, int y) {
@@ -443,6 +470,7 @@ public class DrawInterface extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JLabel jLabel1;
