af87c34b56e0b3f5b8a1ebc943cde09d8e50b849
Maikel Steneker <maikel.steneker@gmail.com>
2015-01-08 13:11:59 +0100

    Bounding box can now also be drawn.

--- src/nl/tue/win/ga/model/drawing/ResultDrawable.java
+++ src/nl/tue/win/ga/model/drawing/ResultDrawable.java
@@ -1,9 +1,10 @@
 package nl.tue.win.ga.model.drawing;
 
-import nl.tue.win.ga.model.drawing.Drawable;
 import java.awt.Graphics;
+import java.util.ArrayList;
 import nl.tue.win.ga.model.LineSegment;
 import nl.tue.win.ga.model.SimplePolygon;
+import nl.tue.win.ga.utilities.BoundingBox;
 import nl.tue.win.ga.utilities.DrawingUtilities;
 
 /**
@@ -16,20 +17,29 @@ public class ResultDrawable implements Drawable {
     private final SimplePolygon polygon;
     private final Iterable<LineSegment> segments;
     private final DrawingUtilities drawingUtilities;
+    private final Iterable<LineSegment> bb;
 
     public ResultDrawable(SimplePolygon polygon, Iterable<LineSegment> segments) {
+        this(polygon,segments, new ArrayList<LineSegment>());
+    }
+    
+    public ResultDrawable(SimplePolygon polygon, Iterable<LineSegment> segments,
+            Iterable<LineSegment> bb) {
         this.polygon = polygon;
         this.segments = segments;
         this.drawingUtilities = polygon.drawingUtilities;
-        for (LineSegment s : segments) {
-            s.drawingUtilities = drawingUtilities;
-        }
+        this.bb = bb;
     }
 
     @Override
     public void draw(Graphics g, boolean scale, boolean invertY) {
         polygon.draw(g, scale, invertY);
         for (LineSegment s : segments) {
+            s.drawingUtilities = drawingUtilities;
+            s.draw(g, scale, invertY);
+        }
+        for (LineSegment s : bb) {
+            s.drawingUtilities = drawingUtilities;
             s.draw(g, scale, invertY);
         }
     }
