c885c04390f8eb9fd96cbf584ad96bc3cf6958d3
CoenHamers <coenhamers@gmail.com>
2015-01-14 11:46:10 +0100

    Works Baby!!! Met mooie regenboog coloring!
    Signed-off-by: CoenHamers <coenhamers@gmail.com>

--- src/nl/tue/win/ga/model/drawing/ResultDrawable.java
+++ src/nl/tue/win/ga/model/drawing/ResultDrawable.java
@@ -2,9 +2,9 @@ package nl.tue.win.ga.model.drawing;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Random;
 import nl.tue.win.ga.model.LineSegment;
 import nl.tue.win.ga.model.SimplePolygon;
-import nl.tue.win.ga.utilities.BoundingBox;
 import nl.tue.win.ga.utilities.DrawingUtilities;
 
 /**
@@ -35,6 +35,12 @@ public class ResultDrawable implements Drawable {
     public void draw(Graphics g, boolean scale, boolean invertY) {
         polygon.draw(g, scale, invertY);
         for (LineSegment s : segments) {
+            float f = new Random().nextFloat();
+            int r1 = new Random().nextInt(1000);
+            int r11 = new Random().nextInt(r1);
+            int r2 = new Random().nextInt(1000);
+            int r22 = new Random().nextInt(r2);
+            g.setColor(java.awt.Color.getHSBColor(f, 1.0f-(float)r11/r1, 1.0f-(float)r22/r2));
             s.drawingUtilities = drawingUtilities;
             s.draw(g, scale, invertY);
         }
