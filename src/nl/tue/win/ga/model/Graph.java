467acfe8787883073737aa334ade9695b9af2ce4
Maikel Steneker <maikel.steneker@gmail.com>
2015-01-17 22:10:42 +0100

    Small fix of assertions.

--- src/nl/tue/win/ga/model/Graph.java
+++ src/nl/tue/win/ga/model/Graph.java
@@ -49,12 +49,13 @@ public class Graph {
                     }
                 }
             } else {
-                throw new IllegalArgumentException("Reached the last else in getFace");
+                assert !DrawInterface.ASSERTIONS:
+                        "Reached the last else in getFace";
             }
         }
         final Face result = check.getFace();
         assert !DrawInterface.ASSERTIONS ||
-                result.contains(p): "Graph returns face that does not contain"
+                result.contains(p): "Graph returns face that does not contain "
                 + "query point (" + result.toString() + " for point " + p + ")";
         return result;
     }
